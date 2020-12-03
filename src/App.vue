b4facd1613e1782910e1ecc7f65d819b7bad3351
Maria Sidiropoulou <maroulisid@gmail.com>
2020-12-03 16:08:21 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Todos 
+      v-bind:todos="todos" 
+      v-on:del-todo="deleteTodo"
+    />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
 export default {
-  name: 'App',
+  name:"app",
   components: {
-    HelloWorld
+    Header,
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Buy gifts",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "Bake cookies",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Decorate house",
+          completed: false
+        },
+        {
+          id: 4,
+          title: "Sent invitations",
+          completed: false
+        },
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todoId !== todo.id)
+    }
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+  body {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+  
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p class="title">
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    width: 70%;
+    margin: 20px auto;
+    padding: 16px;
+    background: #f4f4f4;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+
+  .title {
+    font-size: 18px;
+  }
+
+  .title input {
+    margin-right: 6px;
+  }
+
+  .del {
+    background: #fa4949;
+    color: #fff;
+    border: none;
+    font-size: 12px;
+    padding: 4px 8px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
