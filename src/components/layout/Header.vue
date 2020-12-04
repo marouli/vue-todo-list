7f70b2cec4bb84a8d8821379280eacae82cdde8a
Maria Sidiropoulou <maroulisid@gmail.com>
2020-12-04 15:48:22 +0100

    added vue router

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,6 +2,8 @@
   <header class="header">
     <h1>My Todo List</h1>
     <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
   </header>
 </template>
@@ -19,9 +21,18 @@ export default {
     text-align: center;
     padding: 10px;
   }
+  
   .header a {
     color: #fff;
     padding-right: 5px;
     text-decoration: none;
+  }
+
+  .header a:hover {
+    color: #999;
+  }
+
+  .header a.router-link-exact-active {
+    color: #777;
   }
 </style>
